8a6fa5113384a962cb5d8877c326eff53e658ca0
daichi-koyamaji <pkun63029@gmail.com>
2026-02-06 10:58:57 +0900

    edit cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL")
+     origins(
+      ENV["FRONTEND_URL"],
+      ENV["LOCAL_FRONTEND_URL"]
+    ).compact
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
